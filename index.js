a4b947b6a8085c64bf1b4e3f9f63f96eff802d65
Sunshadeshine <90954539+Sunshadeshine@users.noreply.github.com>
2022-06-30 01:02:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var id=document.getElementById('ans');
+// clearing by clicking on c
+function clear(){
+id.innerHTML="";
+}
+document.querySelector("#clea").addEventListener('click',clear);
+document.getElementsByTagName("td").addEventListener('click',function(event)
+{
+   
+})
